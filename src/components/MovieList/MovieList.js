58111a5d3267e0876efc0f009ba692125ce739a0
yaralahruthik <32637966+yaralahruthik@users.noreply.github.com>
2021-05-02 15:26:43 +0530

    List items

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -1,7 +1,39 @@
+import ListItems from '../UI/ListItems';
+
 const MovieList = () => {
+  const items = [
+		{
+			id: 'e1',
+			title: 'Interstellar',
+			imgURL: `${process.env.PUBLIC_URL}/assets/images/items/interstellar.jpg`,
+			rating: 9,
+		},
+		{
+			id: 'e2',
+			title: 'Inception',
+			imgURL: `${process.env.PUBLIC_URL}/assets/images/items/inception.jpg`,
+			rating: 8,
+		},
+		{
+			id: 'e3',
+			title: 'Dunkirk',
+			imgURL: `${process.env.PUBLIC_URL}/assets/images/items/dunkirk.jpg`,
+			rating: 7,
+		},
+		{
+			id: 'e4',
+			title: 'Tenet',
+			imgURL: `${process.env.PUBLIC_URL}/assets/images/items/tenet.jpg`,
+			rating: 8,
+		},
+	];
+
   return (
-    <div>MovieList</div>
-  );
+		<div className='movieList'>
+			<h2 className='movieList__header'>Movies</h2>
+			<ListItems items={items}/>
+		</div>
+	);
 }
 
 export default MovieList;
