461ed9ec55ed13a4a512d8537e15687447248282
yaralahruthik <32637966+yaralahruthik@users.noreply.github.com>
2021-05-17 16:14:15 +0530

    iframe fix

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -15,7 +15,7 @@ const MovieList = (props) => {
 	return (
 		<div className='movieList'>
 			<h2 className='movieList__header'>{props.title}</h2>
-			<div className='listItems__video'>
+			<div className='videoModal'>
 				{modalShow && <FetchVideo mediaType='movie' id={id} />}
 			</div>
 			<div className='movieList__list'>
--- src/components/TVList/TVList.js
+++ src/components/TVList/TVList.js
@@ -15,7 +15,7 @@ const TVList = (props) => {
 	return (
 		<div className='tvList'>
 			<h2 className='tvList__header'>{props.title}</h2>
-			<div className='listItems__video'>
+			<div className='videoModal'>
 				{modalShow && <FetchVideo mediaType='tv' id={id} />}
 			</div>
 			<div className='tvList__list'>
